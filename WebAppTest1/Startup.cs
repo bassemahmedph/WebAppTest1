1fd3bd6ea088a49e524f27fea04bc7b6f5223a9b
Bassem Ahmed <bassemahmedph>
2020-07-15 16:06:55 +0200

    Add project files.

---
+++ WebAppTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppTest1.Startup))]
+namespace WebAppTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
